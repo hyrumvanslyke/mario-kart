fe161b0d55164f64f8204962d7a8fdcfaf9c8740
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-09-02 20:45:17 -0400

    Need to figure out why line 38 is broke

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 let cardCount = 0;
 const playerTallies = {}; // Object to store player tally counts
 
-document.getElementById('gameForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    createGameCard();
-});
-
+// Function to create a new game card
 function createGameCard() {
     cardCount++;
 
@@ -20,47 +16,7 @@ function createGameCard() {
         <img src="${gameImageURL}" alt="${gameTitle}" width="200">
         <h3>${gameTitle}</h3>
         <div class="player-tally">
-            <div>
-                <p>Hyrum</p>
-                <button onclick="incrementTally('hyrum')">+</button>
-                <span id="hyrumTally${cardCount}">0</span>
-            </div>
-            <div>
-                <p>Killian</p>
-                <button onclick="incrementTally('killian')">+</button>
-                <span id="killianTally${cardCount}">0</span>
-            </div>
-            <div>
-                <p>Anne</p>
-                <button onclick="incrementTally('anne')">+</button>
-                <span id="anneTally${cardCount}">0</span>
-            </div>
-            <div>
-                <p>Roger</p>
-                <button onclick="incrementTally('roger')">+</button>
-                <span id="rogerTally${cardCount}">0</span>
-            </div>
-            <div>
-                <p>Ella</p>
-                <button onclick="incrementTally('ella')">+</button>
-                <span id="ellaTally${cardCount}">0</span>
-            </div>
-            <div>
-                <p>Miri</p>
-                <button onclick="incrementTally('miri')">+</button>
-                <span id="miriTally${cardCount}">0</span>
-            </div>
-            <div>
-                <p>David</p>
-                <button onclick="incrementTally('david')">+</button>
-                <span id="davidTally${cardCount}">0</span>
-            </div>
-            <div>
-                <p>Josh</p>
-                <button onclick="incrementTally('josh')">+</button>
-                <span id="joshTally${cardCount}">0</span>
-            </div>
-            <!-- Add similar markup for other players -->
+            <!-- ... Player tally HTML ... -->
         </div>
     `;
 
@@ -71,9 +27,8 @@ function createGameCard() {
     // Initialize tally counts for this card
     initializeTallyCount();
 
-    // Clear the form fields
-    document.getElementById('gameTitle').value = '';
-    document.getElementById('gameImage').value = '';
+    // Save the card data to Local Storage
+    saveCardDataToLocalStorage(gameCard.outerHTML);
 }
 
 // Function to initialize tally counts for the current card
@@ -88,6 +43,41 @@ function initializeTallyCount() {
 function incrementTally(playerName) {
     playerTallies[playerName][cardCount]++;
     document.getElementById(`${playerName}Tally${cardCount}`).textContent = playerTallies[playerName][cardCount];
+    
+    // Call the save function to update Local Storage
+    savePlayerTalliesToLocalStorage();
+}
+
+// Function to save player tallies to Local Storage
+function savePlayerTalliesToLocalStorage() {
+    localStorage.setItem('playerTallies', JSON.stringify(playerTallies));
+}
+
+// Function to save the card data to Local Storage
+function saveCardDataToLocalStorage(cardData) {
+    // Use a unique key for each card, e.g., 'gameCard1', 'gameCard2', etc.
+    const cardKey = `gameCard${cardCount}`;
+    localStorage.setItem(cardKey, cardData);
+}
+
+// Function to load card data from Local Storage and populate the container
+function loadCardDataFromLocalStorage() {
+    const gameContainer = document.getElementById('gameContainer');
+
+    // Loop through card keys (e.g., 'gameCard1', 'gameCard2', ...) and load the card data
+    for (let i = 1; i <= cardCount; i++) {
+        const cardKey = `gameCard${i}`;
+        const cardData = localStorage.getItem(cardKey);
+
+        if (cardData) {
+            // Create a new game card element and populate it with the saved data
+            const gameCard = document.createElement('div');
+            gameCard.innerHTML = cardData;
+
+            // Add the game card to the container
+            gameContainer.appendChild(gameCard);
+        }
+    }
 }
 
 // Initialize tally counts for all players
@@ -99,3 +89,8 @@ for (const playerName of players) {
 
 // Initial setup for tally counts (Game Card 0)
 initializeTallyCount();
+
+// Call the load function to load saved tallies and card data when the page loads
+loadPlayerTalliesFromLocalStorage();
+loadCardDataFromLocalStorage();
+
